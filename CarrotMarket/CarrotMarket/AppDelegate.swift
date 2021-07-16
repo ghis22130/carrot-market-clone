0db2b91f2b4eb856b60a49b3d1f599795a0aef39
Jisoo Kim <ghis22130@naver.com>
2021-07-16 11:13:30 +0900

    chore: 프로젝트 생성 및 초기 셋팅

---
+++ CarrotMarket/CarrotMarket/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CarrotMarket
+//
+//  Created by 지북 on 2021/07/16.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
